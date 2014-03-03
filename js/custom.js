5ba9dfea804b66b8520d72b4ff249c41615305bb
lesleyburr <burrl@bc.edu>
2014-03-03 10:50:52 -0600

    added slide toggle to saturday 12pm and 1pm tours

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,11 @@
 // SHOW Saturday 12:00 p.m. tours ON CLICK
 // HIDE Saturday 12:00 p.m. tours ON ClICK
-$(".schedule__accordion").click(function() {
-	$(".schedule__time--saturday-12pm").slideToggle( "slow", "linear" );
+$("#schedule__accordion--saturday-12pm").click(function() {
+	$("#schedule__time--saturday-12pm").slideToggle( "slow", "linear" );
+});
+
+// SHOW Saturday 1:00 p.m. tours ON CLICK
+// HIDE Saturday 1:00 p.m. tours ON ClICK
+$("#schedule__accordion--saturday-1pm").click(function() {
+	$("#schedule__time--saturday-1pm").slideToggle( "slow", "linear" );
 });
