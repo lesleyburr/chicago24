622aca4f9297b6d3b72f2fbda2ad1284162b8cba
lesleyburr <burrl@bc.edu>
2014-03-04 15:03:52 -0600

    restructured tour schedule menus for saturday and sunday for large screens

--- js/chicago24.js
+++ js/chicago24.js
@@ -5,51 +5,51 @@
 	// HIDE Tours ON ClICK
 
 $("#Saturday12pm").click(function() {
-	$("#TourSaturday, #ToursSaturday12pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday12pm").slideToggle( "slow", "linear" );
 });
 
 $("#Saturday1pm").click(function() {
-	$("#TourSaturday, #ToursSaturday1pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday1pm").slideToggle( "slow", "linear" );
 });
 
 $("#Saturday2pm").click(function() {
-	$("#TourSaturday, #ToursSaturday2pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday2pm").slideToggle( "slow", "linear" );
 });
 
 $("#Saturday3pm").click(function() {
-	$("#TourSaturday, #ToursSaturday3pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday3pm").slideToggle( "slow", "linear" );
 });
 
 $("#Saturday4pm").click(function() {
-	$("#TourSaturday, #ToursSaturday4pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday4pm").slideToggle( "slow", "linear" );
 });
 
 $("#Saturday5pm").click(function() {
-	$("#TourSaturday, #ToursSaturday5pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday5pm").slideToggle( "slow", "linear" );
 });
 
 $("#Saturday6pm").click(function() {
-	$("#TourSaturday, #ToursSaturday6pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday6pm").slideToggle( "slow", "linear" );
 });
 
 $("#Saturday8pm").click(function() {
-	$("#TourSaturday, #ToursSaturday8pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday8pm").slideToggle( "slow", "linear" );
 });
 
 $("#Saturday10pm").click(function() {
-	$("#TourSaturday, #ToursSaturday10pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday10pm").slideToggle( "slow", "linear" );
 });
 
 $("#Sunday12am").click(function() {
-	$("#TourSunday, #ToursSunday12am").slideToggle( "slow", "linear" );
+	$("#ToursSunday12am").slideToggle( "slow", "linear" );
 });
 
 $("#Sunday4am").click(function() {
-	$("#TourSunday, #ToursSunday4am").slideToggle( "slow", "linear" );
+	$("#ToursSunday4am").slideToggle( "slow", "linear" );
 });
 
 $("#Sunday6am").click(function() {
-	$("#TourSunday, #ToursSunday6am").slideToggle( "slow", "linear" );
+	$("#ToursSunday6am").slideToggle( "slow", "linear" );
 });
 
 $("#Sunday7am").click(function() {
@@ -57,19 +57,19 @@ $("#Sunday7am").click(function() {
 });
 
 $("#Sunday9am").click(function() {
-	$("#TourSunday, #ToursSunday9am").slideToggle( "slow", "linear" );
+	$("#ToursSunday9am").slideToggle( "slow", "linear" );
 });
 
 $("#Sunday10am").click(function() {
-	$("#TourSunday, #ToursSunday10am").slideToggle( "slow", "linear" );
+	$("#ToursSunday10am").slideToggle( "slow", "linear" );
 });
 
 $("#Sunday11am").click(function() {
-	$("#TourSunday, #ToursSunday11am").slideToggle( "slow", "linear" );
+	$("#ToursSunday11am").slideToggle( "slow", "linear" );
 });
 
 $("#Sunday12pm").click(function() {
-	$("#TourSunday, #ToursSunday12pm").slideToggle( "slow", "linear" );
+	$("#ToursSunday12pm").slideToggle( "slow", "linear" );
 });
 
 // 
@@ -78,9 +78,9 @@ $("#Sunday12pm").click(function() {
 	// SHOW Tours ON CLICK
 	// HIDE Tours ON ClICK
 
-$("#ToursScream").click(function() {
-	$(".tour--scream").slideToggle( "slow", "linear" );
-});
+// $("#ToursScream").click(function() {
+// 	$(".tour--scream").slideToggle( "slow", "linear" );
+// });
 
 
 
