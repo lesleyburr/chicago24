c72284622fbea81fdfc043e5dd3cf5d7c0aa275a
lesleyburr <burrl@bc.edu>
2014-03-04 15:24:32 -0600

    working on interactions for time menu on large screens

--- js/chicago24.js
+++ js/chicago24.js
@@ -5,11 +5,22 @@
 	// HIDE Tours ON ClICK
 
 $("#Saturday12pm").click(function() {
-	$("#ToursSaturday12pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday12pm").slideDown( "slow", "linear" );
+		$("#Saturday12pm, #Saturday1pm").click(function() {
+			$("#ToursSaturday12pm").slideUp( "slow", "linear" );
+		});
 });
 
+
+
+
+
+
 $("#Saturday1pm").click(function() {
-	$("#ToursSaturday1pm").slideToggle( "slow", "linear" );
+	$("#ToursSaturday1pm").slideDown( "slow", "linear" );
+		$("#Saturday12pm, #Saturday1pm").click(function() {
+			$("#ToursSaturday1pm").slideUp( "slow", "linear" );
+		});
 });
 
 $("#Saturday2pm").click(function() {
