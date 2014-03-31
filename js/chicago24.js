8fb66b45afc077b249374404520be60e13df24d2
lesleyburr <burrl@bc.edu>
2014-03-31 11:01:26 -0500

    added ghost hunt and midnight bike ride copy

--- js/chicago24.js
+++ js/chicago24.js
@@ -22,31 +22,3 @@ $(".tour__link--read-more").click(function(e) {
 		$(this).parent().siblings().children("[id$=Description]").css('display','inline');
 
 	});
-
-
-
-
-//
-// TOUR PACKAGES
-//
-(function( $ ){
-   $.fn.reading = function() {
-      if ($(this).text() == "… See the tours included") {
-				$(this).text("− Read less");
-			}
-			else {
-				$(this).html("… See the tours included");
-			}
-      return this;
-   };
-})( jQuery );
-
-$(".tour__link--see-tours").click(function(e) {
-		e.preventDefault();
-		$(this).reading();
-		$(this).parent().siblings("p").first().toggleClass("tour__description__excerpt");
-		$(this).parent().siblings("[id$=Info]").slideToggle();
-		$(this).parent().siblings().children("[id$=Description]").slideToggle();
-		$(this).parent().siblings().children("[id$=Description]").css('display','inline');
-
-	});
